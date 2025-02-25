5e5fbddcb9dc3261ce9fb1541755e7d7bdd0d9a8
ramajaffer16 <ramajaffer16@gmail.com>
2025-02-25 16:14:33 +0300

    updates

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -8,14 +8,44 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    
+    let logoImage = UIImageView()
+    let textField = GFTextField()
+    let searchButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemPink
+        configurelogoImage()
+        configuretextField()
+        configuresearchButton()
+        view.backgroundColor = .systemBackground
+    
     }
     
 
+    func configurelogoImage(){
+        view.addSubview(logoImage)
+        logoImage.image = UIImage(named: "gh-logo")
+        logoImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([logoImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),logoImage.heightAnchor.constraint(equalToConstant: 200),logoImage.widthAnchor.constraint(equalToConstant: 200)])
+        
+        
+    }
+    
+    func configuretextField(){
+        view.addSubview(textField)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([textField.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 48),textField.heightAnchor.constraint(equalToConstant: 50),textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant:50),textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50)])
+    }
+    
+    func configuresearchButton(){
+        view.addSubview(searchButton)
+        searchButton.translatesAutoresizingMaskIntoConstraints = false
+       
+        NSLayoutConstraint.activate([searchButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),searchButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),searchButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),searchButton.heightAnchor.constraint(equalToConstant: 50)])
+    }
     
 
 }
