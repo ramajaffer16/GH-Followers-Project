603ffa77d491128458941e2bd00ce2634c5d565e
ramajaffer16 <ramajaffer16@gmail.com>
2025-03-04 11:31:18 +0300

    creating network manager

---
+++ GHFollowers/Managers/NetworkManagers.swift
@@ -0,0 +1,52 @@
+//
+//  NetworkManagers.swift
+//  GHFollowers
+//
+//  Created by RAMA on 03/03/2025.
+//
+
+import UIKit
+class NetworkManager{
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func getFollowers(for username:String, page:Int, completion:@escaping(Result<[FollowersResponse],Error>) -> Void){
+        let Url = "https://api.github.com/users/\(username)/followers"
+        
+        let url = URL(string: Url)!
+        let task = URLSession.shared.dataTask(with: url){ data, response, error in
+            
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "No data Received", code: 0, userInfo: nil)))
+                return
+            }
+            
+            guard let httpresponse = response as? HTTPURLResponse,(200...299).contains(httpresponse.statusCode) else {
+                completion(.failure(NSError(domain: "Invalid Response", code: 1, userInfo: nil)))
+                return
+            }
+            
+            do{
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let FollowersResponse = try decoder.decode([FollowersResponse].self, from: data)
+                completion(.success(FollowersResponse))
+                
+                }catch{
+                    completion(.failure(error))
+                
+            }
+            
+        }
+        task.resume()
+        
+    }
+    
+    
+}
